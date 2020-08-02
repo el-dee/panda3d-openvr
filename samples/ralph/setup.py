3790d624e1b1fdb85b4744e67307b23b286603ac
LD <laurent.deru@gmail.com>
2020-08-02 23:32:33 +0200

    Add bdist_apps example for Ralph demo using local wheel

---
+++ samples/ralph/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="Ralph",
+    options = {
+        'build_apps': {
+            'include_patterns': [
+                '**/*.json',
+                '**/*.png',
+                '**/*.jpg',
+                '**/*.egg',
+                '**/*.pz',
+            ],
+            'package_data_dirs':
+            {
+             'openvr': [('openvr/*.dll', '', {'PKG_DATA_MAKE_EXECUTABLE'}),
+                        ('openvr/*.dylib', '', {'PKG_DATA_MAKE_EXECUTABLE'}),
+                        ('openvr/*.so', '', {'PKG_DATA_MAKE_EXECUTABLE'})],
+            },
+            'gui_apps': {
+                'ralph': 'main.py',
+            },
+            'log_filename': '$USER_APPDATA/Ralph/output.log',
+            'log_append': False,
+            'plugins': [
+                'pandagl',
+                'p3openal_audio',
+            ],
+        }
+    }
+)
